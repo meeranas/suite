bf8e8b9f364eefa9a88810a2494eb31a18d72cf0
meeranas <meersaria01@gmail.com>
2025-12-13 11:23:43 +0500

    fix

---
+++ config/auth.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Configuration
+    |--------------------------------------------------------------------------
+    |
+    | Here you may configure authentication settings for your application.
+    |
+    */
+
+    'jwt_public_key' => env('JWT_PUBLIC_KEY', ''),
+
+];
+
